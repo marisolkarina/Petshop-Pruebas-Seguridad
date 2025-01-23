32b9b92c6c7dc3bd0b7181bc2221167b4095d56a
csotgal <sotog.christian@pucp.edu.pe>
2025-01-22 23:28:51 -0500

    Test Integracionv2

--- test/integration/integration.test.js
+++ test/integration/integration.test.js
@@ -5,30 +5,28 @@ const app = require('../../src/app');
 const Producto = require('../../src/models/producto');
 const { expect } = chai;
 const { ObjectId } = require('mongoose').Types;
+
 const MONGODB_URI = 'mongodb+srv://marisol:secreto@cluster0.71urh.mongodb.net/mascotas?retryWrites=true&w=majority&appName=Cluster0';
 chai.use(chaiHttp);
 
+describe('Test de integración', function() {
+    this.timeout(10000);
 
-describe('Integration Tests', () => {
-
-    //conexion a la bd
-    before((done) => {
-        mongoose.connect(MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-        .then(() => done())
-        .catch((err) => done(err))
+    before(function(done) {
+        mongoose.connect(MONGODB_URI)
+            .then(() => done())
+            .catch(done);
     });
 
-    //desconeccion de la bd
-    after((done) => {
-        mongoose.disconnect()
-        .then(() => done())
+    after(function(done) {
+        mongoose.connection.close()
+            .then(() => {
+                process.exit(0);
+            })
+            .catch(done);
     });
 
-    //limpiar la bd e iniciar con los 2 productos siguientes antes de cada test
-    beforeEach((done) => {
+    beforeEach(function(done) {
         Producto.deleteMany({})
             .then(() => {
                 const productos = [
@@ -40,7 +38,12 @@ describe('Integration Tests', () => {
                         categoria: 'perro',
                         color: 'verde',
                         stock: 10,
-                        idUsuario: new ObjectId()
+                        idUsuario: new ObjectId(),
+                        descuento: {
+                            valor: 0,
+                            fechaExpiracion: new Date()
+                        },
+                        comentarios: []
                     },
                     {
                         nombre: 'Polera Less People More Dogs',
@@ -50,27 +53,53 @@ describe('Integration Tests', () => {
                         categoria: 'gato',
                         color: 'celeste',
                         stock: 10,
-                        idUsuario: new ObjectId()
-                    },
+                        idUsuario: new ObjectId(),
+                        descuento: {
+                            valor: 0,
+                            fechaExpiracion: new Date()
+                        },
+                        comentarios: []
+                    }
                 ];
                 return Producto.insertMany(productos);
             })
             .then(() => done())
-            .catch((err) => done(err));
+            .catch(done);
     });
 
-
-
-    describe('GET /productos', () => {
-        it('should return all products', (done) => {
+    describe('GET /productos', function() {
+        it('Debe devolver todos los productos con la estructura correcta', function(done) {
             chai
-            .request(app)
-            .get('/productos')
-            .end((err, res) => {
-                expect(res).to.have.status(200);
-                done();
-            });
+                .request(app)
+                .get('/productos')
+                .end((err, res) => {
+                    try {
+                        expect(res).to.have.status(200);
+                        expect(res.body).to.be.an('object');
+                        expect(res.body).to.have.property('message', 'Productos de la tienda');
+                        expect(res.body).to.have.property('prods').that.is.an('array');
+                        expect(res.body.prods).to.have.lengthOf(2);
+                        
+                        const producto = res.body.prods[0];
+                        expect(producto).to.have.all.keys(
+                            '_id',
+                            'nombre',
+                            'urlImagen',
+                            'descripcion',
+                            'precio',
+                            'categoria',
+                            'color',
+                            'stock',
+                            'comentarios',
+                            'idUsuario',
+                            'descuento',
+                            '__v'
+                        );
+                        done();
+                    } catch (error) {
+                        done(error);
+                    }
+                });
         });
     });
-
 });
